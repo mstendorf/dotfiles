0aba5f011ceae052307eef294fc6decd7d5a48d7
Martin Stendorf <mstendorf@gmail.com>
2023-09-02 08:54:34 +0200

    changed fileinfo in statusline to show a shortened full path to the file

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -7,7 +7,16 @@ M.ui = {
     },
     statusline = {
         theme = "minimal",
-        separator_style = "round"
+        separator_style = "block",
+        overriden_modules = function(modules)
+            modules[#modules] = require("custom.configs.statusline").cursor_position()
+            -- modules[#modules + 1] = require("custom.configs.statusline").clock()
+            -- modules[#modules] = require("custom.configs.statusline").fileInfo()
+            table.remove(modules, 2)
+            table.insert(modules, 2, require("custom.configs.statusline").fileInfo())
+
+            -- table.insert(modules, 8, require("custom.configs.statusline").showcmd())
+        end,
     }
 }
 M.plugins = "custom.plugins"
