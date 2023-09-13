59dca0295a469c457b7bf6b85ca7bb6793f031b5
Martin Stendorf <mstendorf@masbox.local>
2023-09-13 11:29:53 +0200

    testing out official copilot plugin

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -120,4 +120,15 @@ M.trouble = {
 		},
 	},
 }
+M.copilot = {
+    i = {
+    ["<M-CR>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+      {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+    }
+  }
+}
 return M
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -37,14 +37,27 @@ local plugins = {
         end,
     },
     {
-		-- Copilot plugin
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("custom.configs.copilot")
-		end,
-	},    -- {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+      -- The mapping is set to other key, see custom/lua/mappings
+      -- or run <leader>ch to see copilot mapping section
+    end
+    },
+ --    {
+	-- 	-- Copilot plugin
+	-- 	"zbirenbaum/copilot.lua",
+	-- 	cmd = "Copilot",
+	-- 	event = "InsertEnter",
+	-- 	config = function()
+	-- 		require("custom.configs.copilot")
+	-- 	end,
+	-- },    
+    -- {
     --     "tpope/vim-dadbod",
     --     lazy=false,
     --     dependencies = {
