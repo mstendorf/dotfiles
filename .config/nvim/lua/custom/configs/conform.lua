264b19806a69c1a852acb2cc9da9af4df8b91df8
Martin Stendorf <mstendorf@masbox.local>
2023-10-31 20:03:32 +0100

    added conform config file

---
+++ .config/nvim/lua/custom/configs/conform.lua
@@ -0,0 +1,39 @@
+local options ={
+    formatters ={
+
+        prettier = {
+            extra_args = {
+                "--tab-width=4",
+                "--print-width=110",
+            }
+        },
+
+        golines = {
+            extra_args = {
+                "--max-len=180",
+                "--base-formatter=gofumpt",
+            },
+        }
+    },
+    formatters_by_ft = {
+        lua = {"stylua"},
+        cs = {"csharpier"},
+        javascript = {"prettierd"},
+        go = {"gofmt", "goimports"},
+        tpl = {"djlint"},
+        python = {"black", "isort"},
+        html = {"prettier"},
+        htmldjango = {"prettier"},
+        css = {"prettier"},
+        markdown = {"prettier"},
+        typescript = {"prettier"},
+        typescriptreact = {"prettier"},
+        javascriptreact = {"prettier"},
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true
+    }
+}
+print("conform loaded")
+require("conform").setup(options)
