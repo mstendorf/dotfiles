73114ff18f21f85aeea8c38fcbed2233754fe0a0
Martin Stendorf <mstendorf@masbox.local>
2023-12-19 15:29:26 +0100

    changed statusline to be round

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,27 +1,27 @@
----@type ChadrcConfig 
+---@type ChadrcConfig
 local M = {}
 M.ui = {
-    transparency = true,
-    theme = 'tokyonight',
-    cmp = {
-        style = "atom"
-    },
-    tabufline = {
-        enabled = false,
-    },
-    statusline = {
-        theme = "minimal",
-        separator_style = "block",
-        overriden_modules = function(modules)
-            modules[#modules] = require("custom.configs.statusline").cursor_position()
-            -- modules[#modules + 1] = require("custom.configs.statusline").clock()
-            -- modules[#modules] = require("custom.configs.statusline").fileInfo()
-            table.remove(modules, 2)
-            table.insert(modules, 2, require("custom.configs.statusline").fileInfo())
+  transparency = true,
+  theme = "tokyonight",
+  cmp = {
+    style = "atom",
+  },
+  tabufline = {
+    enabled = false,
+  },
+  statusline = {
+    theme = "minimal",
+    separator_style = "round",
+    overriden_modules = function(modules)
+      modules[#modules] = require("custom.configs.statusline").cursor_position()
+      -- modules[#modules + 1] = require("custom.configs.statusline").clock()
+      -- modules[#modules] = require("custom.configs.statusline").fileInfo()
+      table.remove(modules, 2)
+      table.insert(modules, 2, require("custom.configs.statusline").fileInfo())
 
-            -- table.insert(modules, 8, require("custom.configs.statusline").showcmd())
-        end,
-    }
+      -- table.insert(modules, 8, require("custom.configs.statusline").showcmd())
+    end,
+  },
 }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
