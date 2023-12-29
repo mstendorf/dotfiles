fd7738ac4e0bc19dd346fb2ad8194962c26ab194
Martin Stendorf <mstendorf@gmail.com>
2023-12-29 21:53:29 +0100

    added autoinstall to tresitter, add rust env to path in zsh

--- .config/nvim/lua/custom/configs/telescope.lua
+++ .config/nvim/lua/custom/configs/telescope.lua
@@ -1,29 +1,27 @@
 local M = {}
 
-
 M.opts = {
-    defaults = {
-        prompt_prefix = " ",
-        file_ignore_patterns = {"bin/.*", "obj/.*"},
-        cache_picker = {
-            num_pickers = 5
-        },
-        history = {
-            path = '~/.local/share/nvim/databases/telescope_history',
-            limit = 100,
-        },
-        mappings = {
-            i = {
-                ["<S-Up>"]  = function (...)
-                    require("telescope.actions").cycle_history_next(...)
-                end,
-                ["<S-Down>"] = function (...)
-                    require("telescope.actions").cycle_history_prev(...)
-                end
-            }
-        },
-
+  defaults = {
+    prompt_prefix = " ",
+    file_ignore_patterns = { "bin/.*", "obj/.*", "__pycache__/*" },
+    cache_picker = {
+      num_pickers = 5,
     },
+    history = {
+      path = "~/.local/share/nvim/databases/telescope_history",
+      limit = 100,
+    },
+    mappings = {
+      i = {
+        ["<S-Up>"] = function(...)
+          require("telescope.actions").cycle_history_next(...)
+        end,
+        ["<S-Down>"] = function(...)
+          require("telescope.actions").cycle_history_prev(...)
+        end,
+      },
+    },
+  },
 }
 
 return M
