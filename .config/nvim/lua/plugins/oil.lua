175d940f8f053b289cbee00e7807e4c5accf126f
Martin Stendorf <mas@bnaa.dk>
2024-05-17 12:34:52 +0200

    fixed tmux keybindings in an oil buffer

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,12 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
+    "stevearc/oil.nvim",
+    opts = {
+        keymaps = {
+            ["<C-h>"] = "<cmd> TmuxNavigateLeft<CR>",
+            ["<C-l>"] = "<cmd> TmuxNavigateRight<CR>",
+        },
+    },
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
 }
