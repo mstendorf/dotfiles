d03e5c43cd3742f6b521a5472b9e7f592e7f2710
Martin Stendorf <mstendorf@gmail.com>
2024-06-14 09:27:39 +0200

    made oil settings even more awesome

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,12 +1,22 @@
 return {
-    "stevearc/oil.nvim",
-    opts = {
-        keymaps = {
-            ["<C-h>"] = "<cmd> TmuxNavigateLeft<CR>",
-            ["<C-l>"] = "<cmd> TmuxNavigateRight<CR>",
-        },
-    },
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "VeryLazy",
+	"stevearc/oil.nvim",
+	opts = {
+		keymaps = {
+			["<C-h>"] = "<cmd> TmuxNavigateLeft<CR>",
+			["<C-l>"] = "<cmd> TmuxNavigateRight<CR>",
+		},
+	},
+	config = function()
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name, _)
+					return name == ".." or name == ".git"
+				end,
+			},
+		})
+	end,
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
 }
