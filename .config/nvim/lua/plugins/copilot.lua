0340ebd39d7f43e10183d635e35b6cf238e51199
Martin Stendorf <mstendorf@gmail.com>
2024-08-16 08:15:05 +0200

    Hack to make copilot autostart again. Some gopls tweaks and some visuals

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,14 @@
 return {
-  "github/copilot.vim",
-  -- lazy = false,
-  event = "LspAttach",
-  config = function()
-    -- Mapping tab is already used by NvChad
-    vim.g.copilot_no_tab_map = true
-    vim.g.copilot_assume_mapped = true
-    vim.g.copilot_tab_fallback = ""
-    -- The mapping is set to other key, see custom/lua/mappings
-    -- or run <leader>ch to see copilot mapping section
-  end,
+	"github/copilot.vim",
+	-- lazy = false,
+	event = "LspAttach",
+	config = function()
+		-- Mapping tab is already used by NvChad
+		vim.g.copilot_no_tab_map = true
+		vim.g.copilot_assume_mapped = true
+		vim.g.copilot_tab_fallback = ""
+		-- The mapping is set to other key, see custom/lua/mappings
+		-- or run <leader>ch to see copilot mapping section
+		vim.cmd([[Copilot]])
+	end,
 }
