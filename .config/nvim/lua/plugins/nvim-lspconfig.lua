8a32b8266ef6e9904e688a993fba9004f46a0f79
Martin Stendorf <mas@bnaa.dk>
2024-10-04 10:17:34 +0200

    Changed to new nvchad setup for transparency, fixed some lsp plugin deprecations

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -108,7 +108,7 @@ return {
         --     }
         --   }
         -- }
-        lspconfig.tsserver.setup({
+        lspconfig.ts_ls.setup({
             on_attach = on_attach,
             on_init = on_init,
             capabilities = capabilities,
