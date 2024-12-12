6298e471567ad4f707ff5b6a34ef8c05102ec90d
Martin Stendorf <mas@bnaa.dk>
2024-12-12 09:30:04 +0100

    configured snacks.nvim to fit my needs, added eza replacement for ls

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -100,22 +100,22 @@ return {
             end,
             desc = "Toggle Terminal",
         },
-        {
-            "]]",
-            function()
-                Snacks.words.jump(vim.v.count1)
-            end,
-            desc = "Next Reference",
-            mode = { "n", "t" },
-        },
-        {
-            "[[",
-            function()
-                Snacks.words.jump(-vim.v.count1)
-            end,
-            desc = "Prev Reference",
-            mode = { "n", "t" },
-        },
+        -- {
+        --     "]]",
+        --     function()
+        --         Snacks.words.jump(vim.v.count1)
+        --     end,
+        --     desc = "Next Reference",
+        --     mode = { "n", "t" },
+        -- },
+        -- {
+        --     "[[",
+        --     function()
+        --         Snacks.words.jump(-vim.v.count1)
+        --     end,
+        --     desc = "Prev Reference",
+        --     mode = { "n", "t" },
+        -- },
         {
             "<leader>N",
             desc = "Neovim News",
