b576ed4007ae8aae27bdff3a715419a33d3514a8
Martin Stendorf <mstendorf@gmail.com>
2025-01-06 08:57:07 +0100

    some harpoon tweaks

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -4,7 +4,15 @@ return {
 	event = "VeryLazy",
 	config = function()
 		local harpoon = require("harpoon")
-		harpoon:setup()
+		harpoon:setup({
+			settings = {
+				save_on_toggle = true,
+				sync_on_ui_close = true,
+				key = function()
+					return vim.loop.cwd()
+				end,
+			},
+		})
 
 		vim.keymap.set("n", "<leader>m", function()
 			harpoon:list():add()
@@ -36,5 +44,29 @@ return {
 		vim.keymap.set("n", "<leader><C-;>", function()
 			harpoon:list():replace_at(4)
 		end)
+
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				})
+				:find()
+		end
+
+		vim.keymap.set("n", "<C-e>", function()
+			toggle_telescope(harpoon:list())
+		end, { desc = "Open harpoon window" })
 	end,
 }
