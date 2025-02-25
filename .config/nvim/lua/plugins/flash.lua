e4ea50a319380f34c2046fba2f5b85cd4a4b59be
Martin Stendorf <mas@bnaa.dk>
2025-02-25 08:41:30 +0100

    removed backdrop from flash, to keep syntax highlighting

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -2,7 +2,29 @@ return {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+        highlight = {
+            -- show a backdrop with hl FlashBackdrop
+            backdrop = false,
+            -- Highlight the search matches
+            matches = true,
+            -- extmark priority
+            priority = 5000,
+            groups = {
+                match = "FlashMatch",
+                current = "FlashCurrent",
+                backdrop = "FlashBackdrop",
+                label = "FlashLabel",
+            },
+        },
+        modes = {
+            char = {
+                enabled = true,
+                multi_line = false,
+                highlight = { backdrop = false },
+            },
+        },
+    },
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
