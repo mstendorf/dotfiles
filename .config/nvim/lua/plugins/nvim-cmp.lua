d051f02bbfd32f4c2a479b05b50f03b690a8b93e
Martin Stendorf <mas@bnaa.dk>
2025-03-11 22:30:06 +0100

    made cmp autoselect first element

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,13 +1,16 @@
 return {
-	"hrsh7th/nvim-cmp",
-	-- enabled = false,
-	opts = function(_, conf)
-		conf.mapping = {
-			["<CR>"] = require("cmp").mapping.confirm({ select = true }),
-			["<Tab>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Select }),
-			["<S-Tab>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Select }),
-			["<Down>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Select }),
-			["<Up>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Select }),
-		}
-	end,
+    "hrsh7th/nvim-cmp",
+    -- enabled = false,
+    opts = function(_, conf)
+        conf.mapping = {
+            ["<CR>"] = require("cmp").mapping.confirm({ select = true }),
+            ["<Tab>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Select }),
+            ["<S-Tab>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Select }),
+            ["<Down>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Select }),
+            ["<Up>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Select }),
+        }
+        conf.completion = {
+            completeopt = "menu,menuone,noinsert",
+        }
+    end,
 }
