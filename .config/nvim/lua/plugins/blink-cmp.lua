b762bc2cbcb8b43f8ca7921a86ab5390598fffd0
Martin Stendorf <mas@bnaa.dk>
2025-04-24 13:46:12 +0200

    fixed blink ghost text, made copilot.lua behave

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,22 +1,24 @@
 return {
-	{ import = "nvchad.blink.lazyspec" },
-	{
-		"saghen/blink.cmp",
-		version = "*",
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			keymap = {
-				["<CR>"] = { "accept", "fallback" },
-				["<S-Tab>"] = { "select_prev", "fallback" },
-				["<Tab>"] = { "select_next", "fallback" },
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f"] = { "scroll_documentation_down", "fallback" },
-			},
-
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
-			},
-		},
-	},
+    { import = "nvchad.blink.lazyspec" },
+    {
+        "saghen/blink.cmp",
+        version = "*",
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = {
+                ["<CR>"] = { "accept", "fallback" },
+                ["<S-Tab>"] = { "select_prev", "fallback" },
+                ["<Tab>"] = { "select_next", "fallback" },
+                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+                ["<C-f"] = { "scroll_documentation_down", "fallback" },
+            },
+            completion = {
+                ghost_text = { enabled = false },
+            },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer" },
+            },
+        },
+    },
 }
