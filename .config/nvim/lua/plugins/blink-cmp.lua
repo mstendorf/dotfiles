73a96fd2c73583b9dd3ccd12ca22b459df1ec511
Martin Stendorf <mstendorf@gmail.com>
2025-11-04 11:10:52 +0100

    cleaned up some neovim config

--- .config/nvim/lua/plugins/blink-cmp.lua
+++ .config/nvim/lua/plugins/blink-cmp.lua
@@ -1,36 +1,36 @@
 return {
-    { import = "nvchad.blink.lazyspec" },
-    {
-        "saghen/blink.cmp",
-        version = "*",
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
-        opts = {
-            keymap = {
-                ["<CR>"] = { "accept", "fallback" },
-                ["<S-Tab>"] = { "select_prev", "fallback" },
-                ["<Tab>"] = { "select_next", "fallback" },
-                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-f"] = { "scroll_documentation_down", "fallback" },
-            },
-            signature = {
-                enabled = false,
-                trigger = {
-                    enable = false,
-                },
-            },
-            completion = {
-                ghost_text = { enabled = false },
-                menu = {
-                    draw = {
-                        padding = { 0, 1 },
-                        align_to = "kind_icon",
-                    },
-                },
-            },
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer" },
-            },
-        },
-    },
+	{ import = "nvchad.blink.lazyspec" },
+	{
+		"saghen/blink.cmp",
+		version = "*",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			keymap = {
+				["<CR>"] = { "accept", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<Tab>"] = { "select_next", "fallback" },
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f"] = { "scroll_documentation_down", "fallback" },
+			},
+			signature = {
+				-- enabled = false,
+				trigger = {
+					enable = false,
+				},
+			},
+			completion = {
+				ghost_text = { enabled = false },
+				menu = {
+					draw = {
+						padding = { 0, 1 },
+						align_to = "kind_icon",
+					},
+				},
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+		},
+	},
 }
